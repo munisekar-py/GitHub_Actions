2c176c4b37f2afdb0770bef08a93130bfd0ded9d
munisekar-py <munisheak@gmail.com>
2025-07-20 12:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Jenkins CI/CD!, This is Confirmed WebHooks works fine."
+    return "Hello, GitHubActions CI/CD!, This is Confirmed WebHooks/Triggers works fine."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
