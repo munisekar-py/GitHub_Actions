b476eb45c7b1faabd9d87d43f537073d32dce6ca
munisekar-py <munisheak@gmail.com>
2025-07-20 12:56:35 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello, Jenkins CI/CD!" in response.data
+    assert b"Hello, GitHub Actions CI/CD!" in response.data
